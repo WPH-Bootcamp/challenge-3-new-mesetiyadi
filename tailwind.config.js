9bd2fdd66f8eeeb10565a4608d4a7ec8f8c7ce64
mekos <me.setiyadi27@gmail.com>
2026-05-09 22:12:43 +0700

    masukin .gitignore dan tailwind.config.js ke folder chalenge

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+Module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
